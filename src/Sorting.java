8bf82093ae13087d92a5d255555286408165f073
ABISHEK <abideepu19.vrs@gmail.com>
2025-02-14 00:16:15 +0530

    Quick Sort with the first element as the Pivot

--- src/Sorting.java
+++ src/Sorting.java
@@ -113,12 +113,50 @@ public class Sorting {
 
     }
 
+    private static void quickSort(int[] arr, int low ,int high) {
+        if(low<high){
+            int part = partition(arr,low,high);
+            quickSort(arr,low,part-1);
+            quickSort(arr,part+1,high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (arr[i] <= pivot && i < high - 1) {
+                i++;
+            }
+            while (arr[j] > pivot && j >= low + 1) {
+                j--;
+            }
+            if (i < j) {
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap pivot with arr[j]
+        int temp = arr[j];
+        arr[j] = arr[low];
+        arr[low] = temp;
+
+        return j; // Return the correct pivot index after the swap
+    }
+
     public static void main(String[] args) {
         int[] arr = {4,2,7,1,3};
 //        int[] result = Arrays.stream(arr).distinct().sorted().toArray();
-        mergeSort(arr,0,arr.length-1);
+        quickSort(arr,0,arr.length-1);
         for (int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
     }
+
+
 }
