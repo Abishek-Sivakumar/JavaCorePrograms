d687addb93cc1b31f03e73f454f9d61abaa387e0
ABISHEK <abideepu19.vrs@gmail.com>
2025-02-16 23:57:04 +0530

    Revised Quick Sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -127,10 +127,10 @@ public class Sorting {
         int j = high;
 
         while (i < j) {
-            while (arr[i] <= pivot && i < high - 1) {
+            while (arr[i] <= pivot && i < high) {
                 i++;
             }
-            while (arr[j] > pivot && j >= low + 1) {
+            while (arr[j] >= pivot && j > low ) {
                 j--;
             }
             if (i < j) {
@@ -150,7 +150,7 @@ public class Sorting {
     }
 
     public static void main(String[] args) {
-        int[] arr = {4,2,7,1,3};
+        int[] arr = {4,2,5,1,3};
 //        int[] result = Arrays.stream(arr).distinct().sorted().toArray();
         quickSort(arr,0,arr.length-1);
         for (int i=0;i<arr.length;i++){
