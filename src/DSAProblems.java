39540318c61438166eea9638dbb4db1ec429897b
ABISHEK <abideepu19.vrs@gmail.com>
2026-09-03 08:36:57 +0530

    Added the Word Break 2 problem

--- src/DSAProblems.java
+++ src/DSAProblems.java
@@ -99,6 +99,52 @@ public class DSAProblems {
         System.out.println("The length of substring with distinct characters is : " + maxLen);
     }
 
+    //Leetcode Word Break 1
+    public static boolean wordBreak(String s, String[] dict){
+        Set<String> set = new HashSet<>(Arrays.asList(dict));
+        int n = s.length();
+        boolean dp[] = new boolean[n+1];
+        //VERY IMPORTANT STEP
+        dp[0] = true;
+        //ALSO i MUST Run Till equal to n
+        for(int i = 1; i<=n; i++){
+            for(int j=0;j<i;j++){
+                String temp = s.substring(j, i);
+                if(dp[j] && set.contains(temp)){
+                    dp[i] = true;
+                }
+            }
+        }
+        //THE last value gives the answer whether we can break or not
+        return dp[n];
+    }
+
+    //Leetcode Word Break 2
+    public static List<String> formSentence(String s, String[] dict ){
+        Set<String> set = new HashSet<>(Arrays.asList(dict));
+        int n = s.length();
+        List<String>[] dp = new ArrayList[n+1];
+        for(int i=0;i<=n;i++){
+            dp[i] = new ArrayList<>();
+        }
+        dp[0].add("");
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                String temp = s.substring(j, i);
+                if(dp[j].size() > 0 && set.contains(temp)){
+                    for(String sentence : dp[j]){
+                        if(sentence.isEmpty()){
+                            dp[i].add(temp);
+                        }else{
+                            dp[i].add(sentence + " " + temp);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String args[]){
 //        System.out.println(findLongestCommonSubsequence("abcd", "afgbjhc"));
 //        System.out.println(printLongestCommonSubsequence("abcd", "afgbjhc"));
